5c6b3f7608f41f60eedeb34e209d49caa7211299
Jonatan Antoni <jonatan.antoni@arm.com>
2022-05-31 13:30:43 +0200

    Automatic pack version inference.

--- Scripts/gen_pack.sh
+++ Scripts/gen_pack.sh
@@ -19,6 +19,8 @@ set -o pipefail
 
 DIRNAME=$(dirname $(readlink -f $0))
 CHANGELOG=$(readlink -f ${DIRNAME}/gen_changelog.sh)
+DESCRIBE=$(readlink -f ${DIRNAME}/git_describe.sh)
+VERSION=$(${DESCRIBE} v)
 
 function usage {
   echo "$(basename $0) [-h|--help] [<pdsc>]"
@@ -187,7 +189,8 @@ let first-=1
 let last+=1
 head -n ${first} "./${PACK_VENDOR}.${PACK_NAME}.pdsc" > "${PACK_BUILD}/${PACK_VENDOR}.${PACK_NAME}.pdsc"
 "${CHANGELOG}" -p -f pdsc 2>/dev/null | sed "s/^/  /" >> "${PACK_BUILD}/${PACK_VENDOR}.${PACK_NAME}.pdsc"
-tail -n +${last} "./${PACK_VENDOR}.${PACK_NAME}.pdsc" >> "${PACK_BUILD}/${PACK_VENDOR}.${PACK_NAME}.pdsc"
+tail -n +${last} "./${PACK_VENDOR}.${PACK_NAME}.pdsc" | \
+  sed -e "s/Cversion=\"[^\"]*\"/Cversion=\"${VERSION}\"/" >> "${PACK_BUILD}/${PACK_VENDOR}.${PACK_NAME}.pdsc"
 
 # Add directories
 echo Adding directories to pack:
