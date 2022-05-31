4b3f350570d3ed0c64e7f701d91246b5b84ff512
Jonatan Antoni <jonatan.antoni@arm.com>
2022-05-31 11:45:08 +0200

    Initial contribution of main branch documentation.

---
+++ main/footer.js
@@ -0,0 +1,7 @@
+function writeHeader() {
+    document.write('Version 4.0.1-dev1+g474afab');
+};
+
+function writeFooter()  {
+    document.write('Generated on Tue May 31 2022 09:16:59 for CMSIS-NN 4.0.1-dev1+g474afab. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
+};
